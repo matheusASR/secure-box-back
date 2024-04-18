d8f15c747fd0ec2a28108ae1d1c11f2da3107652
matheussASR <rm551466@fiap.com>
2024-04-18 13:18:39 -0300

    fix: corrigindo rota sendCode

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -80,6 +80,7 @@ const sendCode = async (payload: any): Promise<void> => {
   var transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
+    port: 465,
     auth: {
       user: "regomatheus881@gmail.com",
       pass: process.env.APP_PASSWORD
